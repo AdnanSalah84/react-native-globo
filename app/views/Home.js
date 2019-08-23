865372618bf9f61439d19be9973e4ff60d1a63dd
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-08-23 12:16:50 +0500

    Added home page

---
+++ app/views/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Home extends React.Component() {
+    render() {
+        return (
+            <div>
+                <View>
+                    <Text>This will be the Homepage</Text>
+                    <Text>These other lines are here</Text>
+                    <Text>So you can see the text in the app</Text>
+                </View>
+            </div>
+        )
+    }
+}
