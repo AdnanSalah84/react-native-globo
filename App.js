18640d471ee6cb4cff84c6c94802de632cfbbd94
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-09-02 15:37:45 +0500

    Created navigation and user input

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import React from 'react';
-
 import { Home } from './app/views/Home';
+import { Contact } from './app/views/Contact';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default function App() {
+const MyRoutes = createStackNavigator({
+  HomeRT: {
+    screen: Home
+  },
+  ContactRT: {
+    screen: Contact
+  },
+}, {
+    initialRouteName: 'HomeRT'
+  }
+);
+
+
+const App = createAppContainer(MyRoutes);
+
+
+export default App;
+
+/*export default function App() {
   return (
-    <Home />
+    // <Home />
+    <MyRoutes />
 
   );
-}
+}*/
--- app/views/Home.js
+++ app/views/Home.js
@@ -3,19 +3,31 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Header } from '../sections/Header';
 import { Hero } from '../sections/Hero';
 import { Menu } from '../sections/Menu';
+import { createStackNavigator } from 'react-navigation';
 
-export function Home() {
-    return (
 
-        <View style={styles.container}>
-            <Header message='Press to Login' />
-            {/* <Text style={{ flex: 8 }}>This will be the Homepage</Text> */}
-            {/* <Text style={{ flex: 8 }}>These other lines are here</Text> */}
-            <Hero />
-            <Menu />
-        </View>
 
-    )
+// export class Home extends React.Component() {
+export class Home extends React.Component {
+    static navigationOptions = {
+        header: null
+    }
+
+    render() {
+
+        const { navigate } = this.props.navigation;
+
+        return (
+
+            <View style={styles.container}>
+                <Header message='Press to Login' />
+                {/* <Text style={{ flex: 8 }}>This will be the Homepage</Text> */}
+                {/* <Text style={{ flex: 8 }}>These other lines are here</Text> */}
+                <Hero />
+                <Menu navigate={navigate} />
+            </View>
+        )
+    }
 }
 
 
