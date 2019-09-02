c396329aec2526d740cffd58644487d91072e743
Adnan Salahuddin <adnan.salah84@gmail.com>
2019-09-02 12:15:27 +0500

    Added Images and detecting touch

--- app/views/Home.js
+++ app/views/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from '../sections/Header';
-
+import { Hero } from '../sections/Hero';
+import { Menu } from '../sections/Menu';
 
 export function Home() {
     return (
 
         <View style={styles.container}>
             <Header message='Press to Login' />
-            <Text style={{ flex: 8 }}>This will be the Homepage</Text>
-            <Text style={{ flex: 8 }}>These other lines are here</Text>
+            {/* <Text style={{ flex: 8 }}>This will be the Homepage</Text> */}
+            {/* <Text style={{ flex: 8 }}>These other lines are here</Text> */}
+            <Hero />
+            <Menu />
         </View>
 
     )
